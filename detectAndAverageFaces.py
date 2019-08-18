6ef572bec0ea4c95b5dc9d2fbdc7266afc114e38
johnwmillr <john.w.millr@gmail.com>
2019-08-18 02:06:03 -0400

    Commit for Reddit post

--- detectAndAverageFaces.py
+++ detectAndAverageFaces.py
@@ -2,6 +2,8 @@ from facer import facer
 import sys
 import time
 
+# https://janakiev.com/til/python-background/
+
 args = sys.argv
 if len(args) != 2:
     print()
@@ -16,6 +18,8 @@ print(f"PATH TO PROCESS:\n\t{path_to_images}")
 
 # Load the images
 images = facer.load_images(path_to_images, verbose=True)
+# n = 15
+# images = {key:val for key, val in zip(list(images.keys())[:n], list(images.values())[:n])}
 
 # Detect landmarks for each face
 t0 = time.time()
@@ -26,7 +30,7 @@ print(f"Detecting landmarks took {elapsed:.2f} minutes.")
 # Average the faces together
 t0 = time.time()
 fn = f"average_face_{path_to_images.strip('/').rsplit('/', 1)[-1]}.jpg"
-facer.create_average_face(faces, landmarks, output_file=fn, save_image=True, output_dims=(450, 600))
+facer.create_average_face(faces, landmarks, output_file=fn, save_image=True, output_dims=(600, 600))
 elapsed = (time.time() - t0) / 60
 print(f"Averaging faces took {elapsed:.2f} minutes.")
 
